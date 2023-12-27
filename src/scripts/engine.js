c863f1daf8f8ec418dc7164f43aa8849efc8a485
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-27 06:17:45 -0300

    Correção JavaScript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -29,7 +29,7 @@ function countDown() {
 }
 
 function playSound(audioName) {
-    let audio = new Audio(`../audios/${audioName}.m4a`);
+    let audio = new Audio(`../../src/audios/${audioName}.m4a`);
     audio.volume = 0.2;
     audio.play();
 }
