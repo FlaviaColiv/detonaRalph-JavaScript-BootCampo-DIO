e4e661e2d17014704a3c6f575d34e6f16627f882
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-27 06:08:27 -0300

    Continuação de JavaScript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -29,7 +29,7 @@ function countDown() {
 }
 
 function playSound(audioName) {
-    let audio = new Audio(`../audios/sfx-magic14/${audioName}.mp3`);
+    let audio = new Audio(`../audios/sfx-magic14/${audioName}.m4a`);
     audio.volume = 0.2;
     audio.play();
 }
@@ -52,7 +52,7 @@ function addListenerHitBox() {
             state.values.result++
             state.view.score.textContent = state.values.result;
             state.values.hitPosition = null;
-            playSound("somAcerto");
+            playSound("hit");
            };
         });
     });
